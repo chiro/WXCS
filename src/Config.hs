67c825157c357f159d67045aaaa20013da16f322
Yuichiro Hanada <peanut2626@gmail.com>
2014-02-05 22:03:06 +0900

    Add proxy entry to a configuration file.

--- src/Config.hs
+++ src/Config.hs
@@ -4,7 +4,7 @@ module Config where
 
 import Control.Applicative (empty, (<$>), (<*>))
 
-import qualified Data.Aeson as AE
+import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.Default
 
@@ -13,29 +13,27 @@ import System.Directory (doesFileExist)
 data Configuration = Configuration {
   port :: Int,
   aojUser :: String,
-  aojPass :: String
+  aojPass :: String,
+  proxyHost :: Maybe String,
+  proxyPort :: Maybe Int
   } deriving (Eq, Show)
 
 instance Default Configuration where
   def = Configuration {
     port = 16384,
     aojUser = "",
-    aojPass = ""
+    aojPass = "",
+    proxyHost = Nothing,
+    proxyPort = Nothing
     }
 
-instance AE.ToJSON Configuration where
-  toJSON (Configuration port' user pass) =
-    AE.object [
-      "port" AE..= port',
-      "aojUser" AE..= user,
-      "aojPass" AE..= pass
-      ]
-
-instance AE.FromJSON Configuration where
-  parseJSON (AE.Object v) = Configuration <$>
-                            v AE..: "port" <*>
-                            v AE..: "aojUser" <*>
-                            v AE..: "aojPass"
+instance FromJSON Configuration where
+  parseJSON (Object v) = Configuration <$>
+                            v .: "port" <*>
+                            v .: "aojUser" <*>
+                            v .: "aojPass" <*>
+                            v .:? "proxyHost" <*>
+                            v .:? "proxyPort"
   parseJSON _ = empty
 
 loadConfig :: FilePath -> IO (Maybe Configuration)
@@ -44,4 +42,4 @@ loadConfig filepath = do
   if existp then loadConfig' filepath else return Nothing
   where loadConfig' fp = do
           content <- BL.readFile fp
-          return $ AE.decode content
+          return $ decode content
